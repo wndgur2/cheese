c01896384dc0b240b960fb29f978fd5bb0982657
leejunghyeok <wndgur76@naver.com>
2023-11-02 01:51:40 +0900

    인화시작되었습니다

--- front/app/home/printQueue/page.jsx
+++ front/app/home/printQueue/page.jsx
@@ -2,17 +2,15 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import queueStyle from "../queue.module.css";
-import axios from "axios";
+// import axios from "axios";
 
 export default function PrinterQueue() {
   let router = useRouter();
-  const [queueLength, setQueueLength] = useState(1);
+  // const [queueLength, setQueueLength] = useState(1);
 
   useEffect(()=>{
     let branch = JSON.parse(localStorage.getItem("branch"));
-    if(!branch){
-      router.push("/home/cheeseMap");
-    }
+    if(!branch) router.push("/home/cheeseMap");
     // axios.get(`http://${process.env.NEXT_PUBLIC_API}/printerQueue/${branch.id}`, {
     //   params: { device: localStorage.getItem("uuid") }
     // }).then(res=>{
@@ -25,14 +23,17 @@ export default function PrinterQueue() {
   return (
     <div className="container">
       <div className={queueStyle.textBox}>
-        <span style={{fontWeight: 700}}>인화 대기중</span><span>입니다.</span> <br/>
+        <span style={{fontWeight:700}}>사진 인화</span><span>를 시작했어요!</span> <br/>
+        <span>인화기를 확인하세요.</span>
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "8vh",
+        }}>
+          <img src="/print/printing.gif" width={"100%"}/>
+        </div>
         {/* <span>내 앞에 대기자가</span>&nbsp;
         <span style={{fontWeight: 700}}>{queueLength-1}명</span>&nbsp;<span>있어요.</span> */}
-      </div>
-      <div className={queueStyle.dots}>
-        <div className={queueStyle.dot} id={queueStyle.d1} />
-        <div className={queueStyle.dot} id={queueStyle.d2} />
-        <div className={queueStyle.dot} id={queueStyle.d3} />
       </div>
     </div>
   )
